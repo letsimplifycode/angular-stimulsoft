5eae4b082ad73577cda48ff6e0c0ee8a897ba3e0
Anish Ravindran <30914502+anishravindran@users.noreply.github.com>
2020-08-22 13:11:52 +0530

    Baseline Version V1.0
     Baseline Version V1.0

---
+++ Angular.Reports/Angular.Reports/Controllers/ViewerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Angular;
+using Stimulsoft.Report.Web;
+
+namespace Angular.Reports.Controllers
+{
+    public class ViewerController : Controller
+    {
+        [HttpPost]       
+        public IActionResult InitViewer()
+        {
+            var requestParams = StiAngularViewer.GetRequestParams(this);
+            var options = new StiAngularViewerOptions();
+            options.Actions.ViewerEvent = "ViewerEvent";
+            options.Theme = StiViewerTheme.Office2007Blue;
+            return StiAngularViewer.ViewerDataResult(requestParams, options);
+        }
+
+        //ViewerEvent() that will process viewer requests.
+        [HttpPost] 
+        public IActionResult ViewerEvent()
+        {
+            var requestParams = StiAngularViewer.GetRequestParams(this);
+            if (requestParams.Action == StiAction.GetReport)
+            {
+                var report = StiReport.CreateNewReport();
+                var path = StiAngularHelper.MapPath(this, $"Reports/EmployeeReport.mrt");
+                report.Load(path);
+                return StiAngularViewer.GetReportResult(this, report);
+            }
+            return StiAngularViewer.ProcessRequestResult(this);
+        }
+    }
+}
---
+++ Angular.Reports/Angular.Reports/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Angular.Reports.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
